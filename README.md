46f92feecf19d6cb0ac08b9a557237cb2f6b94c6
marianacondef <marianacondefidelis@gmail.com>
2023-10-22 15:37:20 -0300

    Projeto V1

---
+++ README.md
@@ -0,0 +1,4 @@
+# Projeto Conta no banco por Terminal
+
+## Objetivo
+Pedir informações do usuário via Terminal e imprimir uma mensagem do tipo ""Olá [Nome Cliente], obrigado por criar uma conta em nosso banco, sua agência é [Agencia], conta [Numero] e seu saldo [Saldo] já está disponível para saque".
---
+++ src/ContaTerminal.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência !");
+        int numero = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite a Agência !");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite seu nome !");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite seu saldo !");
+        int saldo = scanner.nextInt();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta " + numero + ", e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
+    }
+}
